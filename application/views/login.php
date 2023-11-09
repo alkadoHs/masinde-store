97a958aefd3a138d37e378b6f9db87f1e922ac36
alkado_hs <alkadosichone@gmail.com>
2023-11-09 06:16:45 +0300

    ...

--- application/views/login.php
+++ application/views/login.php
@@ -16,7 +16,7 @@
 
           Masinde Store    
       </a>
-      <p class="text-slate-400"> Sales & Store Software</p>
+      <p class="text-slate-400 text-lg mb-5"> Sales & Store Software</p>
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
