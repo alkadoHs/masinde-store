b0081a5cd16fc3ed1f0181674bc9be4fd334ee4d
alkado_hs <alkadosichone@gmail.com>
2023-11-22 17:10:22 +0300

    ...

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -117,7 +117,7 @@
                 <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
                 </svg>
-                Profit rate 23.5%
+                <!-- Profit rate 23.5% -->
               </span>
             </div>
           </div>
--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -280,13 +280,13 @@
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >Sellers Reports</a>
               </li>
-              <li>
+              <!-- <li>
                 <a
-                  href="<?= site_url('branchProducts/index/1') ?>"
+                  href="<?//= site_url('branchProducts/index/1') ?>"
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >Branch Products</a
                 >
-              </li>
+              </li> -->
             </ul>
           </li>
           
